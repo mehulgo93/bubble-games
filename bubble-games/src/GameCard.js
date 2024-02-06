41b744582c477335cc337342e1c9072c5382115e
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-02-06 13:41:05 -0500

    changes to the gamecard component

--- bubble-games/src/GameCard.js
+++ bubble-games/src/GameCard.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function GameCard({ game }) {
+    // Default values to handle missing information
+    const imageName = game?.name ?? 'Unknown Game';
+    const imageUrl = game?.image_background ?? 'default_image_url_here';
+    const platformName = game?.platform ?? 'Unknown Platform'; // Assuming you have a 'platform' property
+
     return (
         <div className="card">
-            <img className="img-card" src={game.image_background} alt={game.name} />
-            <h3>{game.games.name}</h3>
-            <h4>Slug: {game.games.slug}</h4>
+            <img className="img-card" src={imageUrl} alt={imageName} />
+            <h2>Name: {imageName}</h2>
+            <h4>Platform: {platformName}</h4>
             <h5><button className="card-button">Add to Wish List</button></h5>
-            <h6>Price: {game.games.price}</h6>
         </div>
-
-    )
-
+    );
 }
+
+// PropType validation
+GameCard.propTypes = {
+    game: PropTypes.shape({
+        image_background: PropTypes.string,
+        name: PropTypes.string.isRequired,
+        platform: PropTypes.string, // Adjust based on the actual structure of your game object
+    })
+};
 
 export default GameCard;
